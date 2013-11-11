4796d0ceb32caebf039619095398c406d5897f7b
gu <guymguym@gmail.com>
2013-11-11 06:14:46 +0200

    update mongo production

--- mongo-production.sh
+++ mongo-production.sh
@@ -1,1 +1,1 @@
-mongo hanso.mongohq.com:10016/app18786653 -u heroku -p 55a9f2a1e6b714379c62e7d3494b0b03
+mongo hanso.mongohq.com:10016/app18786653 -u heroku -p pQPb0rAeh99L7yaeo1OXP-OH-suNiRkDd7onWnilTIRVe8O8K2FCGQE9fwfwne4nKDtX2SVnQoTRFwL78FwYkw 
